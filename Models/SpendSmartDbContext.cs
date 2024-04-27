9a9a6ddfc2ff4cad9dadba036c2fce6c02e6097f
Hania Kołtuniak <hanna.koltuniak@gmail.com>
2024-04-27 11:14:58 +0200

    Add project files.

---
+++ Models/SpendSmartDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SpendSmart.Models
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
